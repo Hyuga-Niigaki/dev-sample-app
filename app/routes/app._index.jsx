5f846555e35779be4d395463f51c6537b5512f57
niigaki03310 <niigaki.hyu@gmail.com>
2024-01-21 17:26:41 +0900

    決済カスタマイズ作成

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,6 +1,12 @@
 import { useEffect } from "react";
 import { json } from "@remix-run/node";
-import { useActionData, useNavigation, useSubmit } from "@remix-run/react";
+import {
+  Form,
+  useActionData,
+  useNavigation,
+  useSubmit,
+  useLoaderData,
+} from "@remix-run/react";
 import {
   Page,
   Grid,
@@ -17,11 +23,26 @@ import {
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
-  await authenticate.admin(request);
-  return null;
+  const { admin } = await authenticate.admin(request);
+  const response = await admin.graphql(
+    `#graphql
+      query{
+shop {
+        name
+        primaryDomain {
+          url
+        }
+      }
+}`
+  );
+
+  return await response.json();
 };
 
 export default function Index() {
+  const loaderData = useLoaderData();
+  console.log("loaderData:", loaderData.data.shop.primaryDomain?.url); // Get domain
+
   return (
     <Page fullWidth>
       <ui-title-bar title="Remix sample app">
@@ -29,15 +50,15 @@ export default function Index() {
       <Grid>
         <Grid.Cell columnSpan={{xs: 6, sm: 3, md: 3, lg: 6, xl: 6}}>
            <Link monochrome url="/app/qrcode">
-            <Card title="Sales" sectioned>
+            <Card sectioned>
               <p>Genarate Qrcodes</p>
             </Card>
           </Link>
         </Grid.Cell>
         <Grid.Cell columnSpan={{xs: 6, sm: 3, md: 3, lg: 6, xl: 6}}>
-          <Link monochrome url="/app">
-            <Card title="Sales" sectioned>
-              <p></p>
+          <Link monochrome url="shopify:admin/settings/payments/customizations">
+            <Card sectioned>
+              <p>Generate Payment Customization</p>
             </Card>
           </Link>
         </Grid.Cell>
